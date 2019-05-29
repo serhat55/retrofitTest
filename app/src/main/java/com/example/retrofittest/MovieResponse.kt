404425790dcaba195abb025ddad5bbae16784608
saliha <saliha@salihas-MacBook-Air.local>
2019-05-29 23:39:41 +0300

    Pojo siniflarinin hepsi eklendi

---
+++ app/src/main/java/com/example/retrofittest/MovieResponse.kt
@@ -0,0 +1,10 @@
+package com.example.retrofittest
+
+import com.google.gson.annotations.SerializedName
+
+class MovieResponse {
+
+    @SerializedName("data")
+    val data : Movies? = null
+
+}
---
+++ app/src/main/java/com/example/retrofittest/Movies.kt
@@ -0,0 +1,10 @@
+package com.example.retrofittest
+
+import com.google.gson.annotations.SerializedName
+
+class Movies {
+
+    @SerializedName("movies")
+    val movies : List<Movie>? = null
+
+}
