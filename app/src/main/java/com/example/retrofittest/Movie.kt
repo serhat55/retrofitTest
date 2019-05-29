b935516e7ca089b8d68ae3ee0928a42b18a1d0b7
saliha <saliha@salihas-MacBook-Air.local>
2019-05-29 23:04:12 +0300

    pojo siniflari olusturuldu

---
+++ app/src/main/java/com/example/retrofittest/Movie.kt
@@ -0,0 +1,25 @@
+package com.example.retrofittest
+
+import com.google.gson.annotations.SerializedName
+
+class Movie {
+
+    @SerializedName("id")
+    val id: Int = 0
+
+    @SerializedName("title")
+    val title: String? = null
+
+    @SerializedName("year")
+    val year: String? = null
+
+    @SerializedName("rating")
+    val rating: String? = null
+
+    @SerializedName("summary")
+    val summary: String? = null
+
+    @SerializedName("image")
+    val image: String? = null
+
+}
